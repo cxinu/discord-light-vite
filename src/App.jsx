68d3993b659cbbbe9034fdc2e46e005a11278e2c
cxinu <cxinu3099@gmail.com>
2022-10-08 14:49:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App ">
+      <p className="text-2xl border-2 border-lime-400 rounded-lg p-2">Hello</p>
+    </div>
+  );
+}
+
+export default App;
